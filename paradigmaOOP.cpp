d255c9ac8ee9c92d9744e47df82879a49ff51ca5
Muh Arya Saputra <aryaom74@gmail.com>
2026-04-21 15:25:40 +0700

    Update paradigmaOOP.cpp

--- paradigmaOOP.cpp
+++ paradigmaOOP.cpp
@@ -11,18 +11,7 @@ class Mahasiswa{
             cout << "Nama: " << nama << endl;
             cout << "Nilai: " << nilai << endl;
         }
- 
-class Mahasiswa{
-    public:
-        int nim;
-        string nama;
-        float nilai;
-        void printData(){
-            cout << "NIM: " << nim << endl;
-            cout << "Nama: " << nama << endl;
-            cout << "Nilai: " << nilai << endl;
-        }
-    };
+
     int main(){
         Mahasiswa mhs1;
 
