4a8e44070ca0b6e82c3579c6a1bbe297ca4974c0
Muh Arya Saputra <aryaom74@gmail.com>
2026-04-21 15:24:50 +0700

    Update paradigmaOOP.cpp

--- paradigmaOOP.cpp
+++ paradigmaOOP.cpp
@@ -11,12 +11,4 @@ class Mahasiswa{
             cout << "Nama: " << nama << endl;
             cout << "Nilai: " << nilai << endl;
         }
-    };
-    int main(){
-        Mahasiswa mhs1;
-
-        mhs1.nim = 1111;
-        mhs1.nama = "Joko susilo";
-        mhs1.nilai = 75.5;
-        mhs1.printData();
-    }
+ 
